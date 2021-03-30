25d32e037575228bc29f917acf69f18068e2009e
YuraNovichkov <80814495+YuraNovichkov@users.noreply.github.com>
2021-03-30 21:41:14 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    int a = faStr1("  Asd    QWe2r 2ew Zxcvbn4 zx As ddd  Yy");
+	int b = faStr2("  Asd    QWe2r 2ew Zxcvbn4 zx As ddd  Yy");
+	int c = faStr3("  Asd    QWe2r 2ew Zxcvbn4 zx As ddd  Yy");
     return 0;
 }
